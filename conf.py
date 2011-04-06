f02259724017a2edd81922636967aeafbd714517
Jyrki Pulliainen <jyrki@taiste.fi>
2011-04-06 22:29:35 +0300

    Add correct URLs for blog

--- conf.py
+++ conf.py
@@ -1,9 +1,24 @@
-from stango.files import Files
-import views
+from stango.files import *
+from .lib.blog import Blog
 
 index_file = 'index.html'
 
+# Views
+def render_template(name, ctx={}):
+    if name.endswith('.md'):
+        pass
+    return lambda context: context.render_template(name, **ctx)
+
+blog = Blog(
+    '', 'blog',
+    default_author='Jyrki Pulliainen',
+    entry_suffix='.html',
+)
+
 files = Files(
-    ('', views.hello, { 'message': 'Hello, World!', 'link': 'greeting.html' }),
-    ('greeting.html', views.hello, { 'message': 'Greetings, World!', 'link': 'index.html' }),
+    blog.files,
+    # Static files
+    files_from_dir('', 'static', strip=1),
+    ('about/', render_template('about.html')),
+    ('projects/', render_template('projects.html')),
 )
