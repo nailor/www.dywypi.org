afd710828f78cef8b5e517ebe9ee1368e6debab4
Jyrki Pulliainen <jyrki@taiste.fi>
2011-04-08 10:26:05 +0300

    Add google analytics

--- conf.py
+++ conf.py
@@ -22,3 +22,35 @@ files = Files(
     ('about/', render_template('about.html')),
     ('projects/', render_template('projects.html')),
 )
+
+
+analytics_script = '''
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22601090-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+'''
+
+def post_render_hook(context, data):
+    # Don't add the analytics script when using the Stango test server
+    if context.mode == 'serving':
+        return data
+
+    name, ext = os.path.splitext(context.realpath)
+    if ext != '.html':
+        return data
+
+    offset = data.find(b'</body>')
+    if offset == -1:
+        return data
+
+    return data[:offset] + analytics_script + data[offset:]
